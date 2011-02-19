56655d912e180ce5c61849c3ddc617fc03d542b2
eroux <eroux@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-02-19 18:43:53 +0000

    Updated Version Number
    git-svn-id: https://plugins.svn.wordpress.org/badge/trunk@347792 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- DisplayBadge.php
+++ DisplayBadge.php
@@ -4,7 +4,7 @@
     Plugin URI: http://www.damn.org.za/blog/DisplayBadge
     Description: Individual Badge Version of DisplayBadges
     Author: Eugéne Roux
-    Version: 1.0
+    Version: 1.1
     Author URI: http://damn.org.za/blog/
  */
 
